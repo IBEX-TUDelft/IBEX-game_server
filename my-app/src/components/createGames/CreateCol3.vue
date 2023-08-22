d9401c32aaf1dd9da63d692240e9c67367d0e2d8
yribero <yary.ribero@toptal.com>
2023-08-22 22:57:46 +0300

    Just a wrapper to simplify form creation

---
+++ my-app/src/components/createGames/CreateCol3.vue
@@ -0,0 +1,10 @@
+<template>
+    <b-col class="form-group col-md-3 d-flex align-items-center">
+        <slot></slot>
+    </b-col>
+</template>
+<script>
+export default {
+    name: 'CreateCol3',
+}
+</script>
