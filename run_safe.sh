f2d5508c863eaf6b4bfa3a06b333aeda7662534a
Yary Ribero <yary.ribero@gmail.com>
2025-08-21 11:08:10 +0300

    typo fixed

--- run_safe.sh
+++ run_safe.sh
@@ -3,6 +3,6 @@
 git checkout feature/ts_support
 cp .env.example .env
 cp api/.env.example api/.env
-cp my_app/.env.example my_app/.env
+cp my-app/.env.example my-app/.env
 chmod 777 db/*
 docker compose -f build.docker-compose.yml up
