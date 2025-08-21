c50258f84425198660522f6a47b558b2a289f853
Yary Ribero <yary.ribero@gmail.com>
2025-08-21 10:56:48 +0300

    Copying all the necessary default examples to files docker has access to, so to initialize the env variables.

--- run_safe.sh
+++ run_safe.sh
@@ -2,5 +2,7 @@
 
 git checkout feature/ts_support
 cp .env.example .env
+cp api/.env.example api/.env
+cp my_app/.env.example my_app/.env
 chmod 777 db/*
 docker compose -f build.docker-compose.yml up
